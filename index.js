a51e2a6381f0bec53665e51a9b52edfd5564bd24
Ben Hubsch <bah37@duke.edu>
2017-07-11 16:11:11 -0700

    bot done

--- index.js
+++ index.js
@@ -3,41 +3,42 @@ var cardObj = require('./config').cardObj
 var customerObj = require('./config').customerObj
 
 var spike = new pizzapi.Customer(customerObj);
+var util = require('util')
 
-console.log("Initializing order...");
-var order = new pizzapi.Order({
-    customer: spike,
-
-    storeID: 7764,
-
-    deliveryMethod: 'Carryout' //('Delivery' or 'Carryout')
-});
-
-console.log("Adding items to order...")
-order.addItem(
-    new pizzapi.Item({
-        code: 'PINPASCA',
-        options: [],
-        quantity: 1,
-    })
-);
-
-console.log("Setting up credit card info...")
-var cardInfo = new order.PaymentObject();
-cardInfo.Amount = order.Amounts.Customer;
-cardInfo.Number = cardObj.num;
-cardInfo.CardType = order.validateCC();
-cardInfo.Expiration = cardObj.exp; // possibly a string?
-cardInfo.SecurityCode = cardObj.sec;
-cardInfo.PostalCode = cardObj.zip; // Billing Zipcode
-
-console.log("Adding card to order...");
-order.Payments.push(cardInfo);
-
-console.log(cardInfo);
-console.log('\n');
-console.log(order);
-
+// console.log("Initializing order...");
+// var order = new pizzapi.Order({
+//     customer: spike,
+//
+//     storeID: 7764,
+//
+//     deliveryMethod: 'Carryout' //('Delivery' or 'Carryout')
+// });
+//
+// console.log("Adding items to order...")
+// order.addItem(
+//     new pizzapi.Item({
+//         code: 'PINPASCA',
+//         options: [],
+//         quantity: 1,
+//     })
+// );
+//
+// console.log("Setting up credit card info...")
+// var cardInfo = new order.PaymentObject();
+// cardInfo.Amount = order.Amounts.Customer;
+// cardInfo.Number = cardObj.num;
+// cardInfo.CardType = order.validateCC();
+// cardInfo.Expiration = cardObj.exp; // possibly a string?
+// cardInfo.SecurityCode = cardObj.sec;
+// cardInfo.PostalCode = cardObj.zip; // Billing Zipcode
+//
+// console.log("Adding card to order...");
+// order.Payments.push(cardInfo);
+//
+// console.log(cardInfo);
+// console.log('\n');
+// console.log(order);
+//
 // order.validate(
 //     function(result) {
 //         console.log("Order has been validated...");
@@ -60,21 +61,22 @@ console.log(order);
 
 
 
-// pizzapi.Util.findNearbyStores(
-//     '1412 Market St., San Francisco, CA',
-//     'Delivery',
-//     function(storeData){
-//         var storeID = storeData.result.Stores[0];
-//         var myStore = new pizzapi.Store({ID: storeID});
-//         myStore.ID = storeData.result.Stores[0].StoreID;
-//         myStore.getMenu(
-//             function(storeData){
-//                 console.log(storeData);
-//             }
-//         );
-//     }
-// );
-
+pizzapi.Util.findNearbyStores(
+    '1412 Market St., San Francisco, CA',
+    'Delivery',
+    function(storeData){
+        var storeID = storeData.result.Stores[0];
+        var myStore = new pizzapi.Store({ID: storeID});
+        myStore.ID = storeData.result.Stores[0].StoreID;
+        myStore.getMenu(
+            function(storeData){
+                // console.log(storeData);
+                console.log(storeData.menuByCode["S_PIZBP"]);
+                console.log(storeData);
+            }
+        );
+    }
+);
 
 
 
