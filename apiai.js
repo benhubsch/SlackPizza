f1c8d73f5d9a19e8cfac4e114a27e1f6e637cfa8
jenshnck <jhonack@sas.upenn.edu>
2017-07-14 15:31:30 -0700

    request fix

--- apiai.js
+++ apiai.js
@@ -1,23 +1,11 @@
-// For each API request, include this HTTP header:
-//
-// Authorization with the value Bearer {access_token}.
-//
-// For example:
-//
-// Authorization: Bearer YOUR_ACCESS_TOKEN
-
-// So, for example, the query requests should be made to the endpoint /query?v=20150910.
-//
-// The v parameter is formatted as YYYYMMDD.
-
 var apiai = require('apiai');
 var axios = require('axios');
 
 var app = apiai(process.env.APIAI_CLI);
 
-var request = function(input) {app.textRequest(input, {
+var request = app.textRequest(input, {
     sessionId: '<unique session id>'
-});}
+})
 
 request.on('response', function(response) {
     console.log( response);
@@ -28,7 +16,3 @@ request.on('error', function(error) {
 });
 
 request.end();
-
-module.exports = {
-    request: request,
-}
