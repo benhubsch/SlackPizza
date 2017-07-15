b63841096bfed53cf37e5b2867ee055d437cffb4
Ben Hubsch <bah37@duke.edu>
2017-07-15 04:05:06 -0700

    fixed untracked files

--- env.sh
+++
@@ -1,5 +0,0 @@
-export SLACK_BOT_TOKEN='xoxb-211251867923-CByaGqytkfmZjIRAhFuqZXyd';
-export GOOGLE_API_KEY='AIzaSyArt4KI1NNnUDZxlDTSR5jvzrx8z7OYmoQ';
-export MONGODB_URI='mongodb://ben:ben@ds123381.mlab.com:23381/ben-slack-pizza';
-export ENCRYPT_KEY="aiUOBr0sCjdt0Wrko2g9";
-export APIAI_CLI='5aa1729dc572457db733f83eb05d2bda';
